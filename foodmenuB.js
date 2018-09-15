23997a285800fb3024757291e68c55ea9fdad06b
ReneFFlores <rer007_@unitec.edu>
2018-09-15 16:13:24 -0600

    b

---
+++ foodmenuB.js
@@ -0,0 +1,295 @@
+{
+    "menu": [
+        {
+            "items": [
+                {
+                    "name": "Infant Burger",
+                    "price": 125,
+                    "discount": 0,
+                    "rating": 3,
+                    "ingredients": [
+                        "buns",
+                        "lettuce",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "b001"
+                },
+                {
+                    "name": "Little Boy Burger",
+                    "price": 150,
+                    "discount": 10,
+                    "rating": 4,
+                    "ingredients": [
+                        "buns",
+                        "lettuce",
+                        "cheeze",
+                        "diced chicken",
+                        "olives",
+                        "mint sauce",
+                        "caeser dressing"
+                    ],
+                    "id": "b002"
+                },
+                {
+                    "name": "Big Boy Burger",
+                    "price": 175,
+                    "discount": 20,
+                    "rating": 5,
+                    "ingredients": [
+                        "buns",
+                        "lettuce",
+                        "cheeze",
+                        "chicken meatballs",
+                        "olives",
+                        "mint sauce",
+                        "caeser dressing"
+                    ],
+                    "id": "b003"
+                },
+                {
+                    "name": "Mid-life Crisis Burger",
+                    "price": 150,
+                    "discount": 15,
+                    "rating": 2,
+                    "ingredients": [
+                        "buns",
+                        "lettuce",
+                        "roasted chicken",
+                        "caeser dressing"
+                    ],
+                    "id": "b004"
+                },
+                {
+                    "name": "Better Take Your Pills Burger",
+                    "price": 115,
+                    "discount": 5,
+                    "rating": 1,
+                    "ingredients": [
+                        "buns",
+                        "lettuce",
+                        "vitamin B"
+                    ],
+                    "id": "b005"
+                },
+                {
+                    "name": "Classic Veg Grilled Sandwitches",
+                    "price": 125,
+                    "discount": 10,
+                    "rating": 4,
+                    "ingredients": [
+                        "bread",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "s001"
+                },
+                {
+                    "name": "Cajun Chicken Burrito",
+                    "price": 150,
+                    "discount": 14,
+                    "rating": 2,
+                    "ingredients": [
+                        "bread",
+                        "cheeze",
+                        "olives",
+                        "crispy cajun chicken"
+                    ],
+                    "id": "s002"
+                },
+                {
+                    "name": "Shredded Lamb Taco",
+                    "price": 175,
+                    "discount": 12,
+                    "rating": 1,
+                    "ingredients": [
+                        "bread",
+                        "lettuce",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "lamb chops"
+                    ],
+                    "id": "s003"
+                },
+                {
+                    "name": "Chicken Shwarma Roll",
+                    "price": 150,
+                    "discount": 30,
+                    "rating": 4,
+                    "ingredients": [
+                        "bread",
+                        "lettuce",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "roasted chicken"
+                    ],
+                    "id": "s004"
+                },
+                {
+                    "name": "Maxican Delight Sandwitch",
+                    "price": 115,
+                    "discount": 25,
+                    "rating": 3,
+                    "ingredients": [
+                        "bread",
+                        "lettuce",
+                        "cheeze",
+                        "ground nuts",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "s005"
+                },
+                {
+                    "name": "Cheeze Pizza",
+                    "price": 125,
+                    "discount": 15,
+                    "rating": 4,
+                    "ingredients": [
+                        "flat bread",
+                        "tomatoes",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "p001"
+                },
+                {
+                    "name": "Veg Pizza",
+                    "price": 150,
+                    "discount": 5,
+                    "rating": 4,
+                    "ingredients": [
+                        "flat bread",
+                        "tomatoes",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "p002"
+                },
+                {
+                    "name": "Chicken Aroma Pizza",
+                    "price": 175,
+                    "discount": 18,
+                    "rating": 4,
+                    "ingredients": [
+                        "flat bread",
+                        "tomatoes",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "p003"
+                },
+                {
+                    "name": "Chicken Tikka Pizza",
+                    "price": 150,
+                    "discount": 22,
+                    "rating": 4,
+                    "ingredients": [
+                        "flat bread",
+                        "tomatoes",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "p004"
+                },
+                {
+                    "name": "Thin-crust King Pizza",
+                    "price": 115,
+                    "discount": 4,
+                    "rating": 4,
+                    "ingredients": [
+                        "flat bread",
+                        "tomatoes",
+                        "cheeze",
+                        "olives",
+                        "red sauce",
+                        "pineaple cubes"
+                    ],
+                    "id": "p005"
+                },
+                {
+                    "name": "Latte",
+                    "price": 125,
+                    "discount": 3,
+                    "rating": 4,
+                    "ingredients": [
+                        "milk",
+                        "caramel",
+                        "coffee beans",
+                        "melted dark chocolate",
+                        "cane sugar"
+                    ],
+                    "id": "d001"
+                },
+                {
+                    "name": "Americano",
+                    "price": 150,
+                    "discount": 14,
+                    "rating": 4,
+                    "ingredients": [
+                        "water",
+                        "coffee beans",
+                        "chocolate chips",
+                        "cane sugar"
+                    ],
+                    "id": "d002"
+                },
+                {
+                    "name": "Cappucino",
+                    "price": 175,
+                    "discount": 8,
+                    "rating": 4,
+                    "ingredients": [
+                        "milk",
+                        "vanila",
+                        "coffee beans",
+                        "melted dark chocolate",
+                        "cane sugar"
+                    ],
+                    "id": "d003"
+                },
+                {
+                    "name": "Espresso",
+                    "price": 150,
+                    "discount": 5,
+                    "rating": 4,
+                    "ingredients": [
+                        "water",
+                        "coffee beans",
+                        "ultra dark chocolate",
+                        "cane sugar"
+                    ],
+                    "id": "d004"
+                },
+                {
+                    "name": "Macchiato",
+                    "price": 115,
+                    "discount": 50,
+                    "rating": 4,
+                    "ingredients": [
+                        "milk",
+                        "caramel",
+                        "coffee beans",
+                        "melted white chocolate",
+                        "cane sugar"
+                    ],
+                    "id": "d005"
+                }
+            ]
+        }
+    ]
+}
