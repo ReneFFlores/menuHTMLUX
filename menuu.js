1de3330feae61b92c0f359562bc44e9028d2be1b
ReneFFlores <rer007_@unitec.edu>
2018-09-20 20:41:22 -0600

    n
    kn

--- menuu.js
+++ menuu.js
@@ -42,7 +42,7 @@
                     "id": "b003"
                 },
                 {
-                    "name": "Mid-life Crisis Burger",
+                    "name": "Sopa Misteriosa",
                     "price": 150,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
@@ -54,7 +54,7 @@
                     "id": "b004"
                 },
                 {
-                    "name": "Better Take Your Pills Burger",
+                    "name": "Chilaquiles",
                     "price": 115,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
@@ -65,33 +65,32 @@
                     "id": "b005"
                 },
                 {
-                    "name": "Classic Veg Grilled Sandwitches",
+                    "name": "Pollo frito",
                     "price": 125,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
-                        "bread",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
+                        "2 piezas de pollo",
+                        "1 complemento",
+                        "1 refresco",
+                        "1 panecillo"
                     ],
                     "id": "s001"
                 },
                 {
-                    "name": "Cajun Chicken Burrito",
-                    "price": 150,
+                    "name": "Ensalada Cesar",
+                    "price": 200,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
-                        "bread",
-                        "cheeze",
-                        "olives",
-                        "crispy cajun chicken"
+                        "Ensalada verde",
+                        "Aderezo Cesar",
+                        "Crutones",
+                        "1 bebida grande"
                     ],
                     "id": "s002"
                 },
                 {
                     "name": "Shredded Lamb Taco",
-                    "price": 175,
+                    "price": 200,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
