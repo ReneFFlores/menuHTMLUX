7a21c62630ea7a079058f4753088f3ef14bd24e8
ReneFFlores <rer007_@unitec.edu>
2018-09-20 20:34:34 -0600

    f
    e

--- menuu.js
+++ menuu.js
@@ -4,21 +4,19 @@
             "categoryName": "Combos",
             "items": [
                 {
-                    "name": "Infant Burger",
+                    "name": "Hamburguesa infantil",
                     "price": 125,
-                    "discount": 0,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
-                        "hamburguesa tamaño S",
+                        "hamburguesa tamaño pequeño",
                         "papas fritas",
                         "refresco"
                     ],
                     "id": "b001"
                 },
                 {
-                    "name": "Sorpresa Boy Burger",
+                    "name": "Sorpresa Burger",
                     "price": 150,
-                    "discount": 10,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "Hamburguesa con queso",
@@ -29,9 +27,8 @@
                     "id": "b002"
                 },
                 {
-                    "name": "Big Boy Burger",
-                    "price": 175,
-                    "discount": 20,
+                    "name": "Baleadas",
+                    "price": 75,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "buns",
@@ -47,7 +44,6 @@
                 {
                     "name": "Mid-life Crisis Burger",
                     "price": 150,
-                    "discount": 15,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "buns",
@@ -60,7 +56,6 @@
                 {
                     "name": "Better Take Your Pills Burger",
                     "price": 115,
-                    "discount": 5,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "buns",
@@ -72,7 +67,6 @@
                 {
                     "name": "Classic Veg Grilled Sandwitches",
                     "price": 125,
-                    "discount": 10,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
@@ -86,7 +80,6 @@
                 {
                     "name": "Cajun Chicken Burrito",
                     "price": 150,
-                    "discount": 14,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
@@ -99,7 +92,6 @@
                 {
                     "name": "Shredded Lamb Taco",
                     "price": 175,
-                    "discount": 12,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
@@ -114,7 +106,6 @@
                 {
                     "name": "Chicken Shwarma Roll",
                     "price": 150,
-                    "discount": 30,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
@@ -129,7 +120,6 @@
                 {
                     "name": "Maxican Delight Sandwitch",
                     "price": 115,
-                    "discount": 25,
                     "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
                     "ingredients": [
                         "bread",
@@ -140,81 +130,6 @@
                         "pineaple cubes"
                     ],
                     "id": "s005"
-                },
-                {
-                    "name": "Cheeze Pizza",
-                    "price": 125,
-                    "discount": 15,
-                    "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
-                    "ingredients": [
-                        "flat bread",
-                        "tomatoes",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
-                    ],
-                    "id": "p001"
-                },
-                {
-                    "name": "Veg Pizza",
-                    "price": 150,
-                    "discount": 5,
-                    "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
-                    "ingredients": [
-                        "flat bread",
-                        "tomatoes",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
-                    ],
-                    "id": "p002"
-                },
-                {
-                    "name": "Chicken Aroma Pizza",
-                    "price": 175,
-                    "discount": 18,
-                    "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
-                    "ingredients": [
-                        "flat bread",
-                        "tomatoes",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
-                    ],
-                    "id": "p003"
-                },
-                {
-                    "name": "Chicken Tikka Pizza",
-                    "price": 150,
-                    "discount": 22,
-                    "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
-                    "ingredients": [
-                        "flat bread",
-                        "tomatoes",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
-                    ],
-                    "id": "p004"
-                },
-                {
-                    "name": "Thin-crust King Pizza",
-                    "price": 115,
-                    "discount": 4,
-                    "url": "https://i.pinimg.com/originals/e6/d5/b9/e6d5b99e200510212d8e0f4e8fdb5043.jpg",
-                    "ingredients": [
-                        "flat bread",
-                        "tomatoes",
-                        "cheeze",
-                        "olives",
-                        "red sauce",
-                        "pineaple cubes"
-                    ],
-                    "id": "p005"
                 }
             ]
         }
